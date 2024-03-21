9f8f3569fbff64cc0072cd6a9e1584e671eed9cc
thiagomcampos1 <72812665+thiagomcampos1@users.noreply.github.com>
2024-03-21 00:45:32 -0300

    cornogay

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,45 +1,47 @@
+import { Link } from 'react-router-dom';
+
 interface BoxProps {
   title: string;
   image: string;
-  onClick?: () => void;
 }
 
 export default function Box({ title, image }: BoxProps) {
- 
-  
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "6em",
-        height: "6em",
-        borderRadius: "0.5em",
-        justifyContent: "center",
-        alignItems: "flex-start",
-        boxShadow: "0 0 0.5em rgba(0, 0, 0, 0.2)",
-        color: "black",
-        flexDirection: "column",
-        padding: "0 1em",
-        gap: "0.5em",
-        cursor: "pointer",
-      }}
-    >
-      <img
-        src={image}
-        alt="maleta"
-        style={{ maxWidth: "1.4em", opacity: 0.5 }}
-      />
-      <p
+    <Link to="./comprar" style={{ textDecoration: 'none', color: 'inherit' }}>
+      <div
         style={{
-          fontSize: "clamp(0.8em, 0.8vw, 1em)",
-          opacity: 0.5,
+          display: "flex",
+          width: "6em",
+          height: "6em",
+          borderRadius: "0.5em",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          boxShadow: "0 0 0.5em rgba(0, 0, 0, 0.2)",
+          color: "black",
+          flexDirection: "column",
+          padding: "0 1em",
+          gap: "0.5em",
+          cursor: "pointer",
         }}
       >
-        {title}
-      </p>
-    </div>
+        <img
+          src={image}
+          alt="maleta"
+          style={{ maxWidth: "1.4em", opacity: 0.5 }}
+        />
+        <p
+          style={{
+            fontSize: "clamp(0.8em, 0.8vw, 1em)",
+            opacity: 0.5,
+          }}
+        >
+          {title}
+        </p>
+      </div>
+    </Link>
   );
 }
+
 export function FullWidthBox({ title, subtitle }: { title: string, subtitle: string }) {
   return (
     <div
@@ -88,7 +90,5 @@ export function FullWidthBox({ title, subtitle }: { title: string, subtitle: str
         Adicionar voo
       </button>
     </div>
-
   );
 }
-
